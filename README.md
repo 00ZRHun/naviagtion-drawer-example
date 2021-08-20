fa46a78a3ff434b049aba0c524677893aa9eaa00
00ZRHun <00zrhun@gmail.com>
2021-08-20 09:51:34 +0800

    Navigation Drawer with Fragments Part 2 - LAYOUT AND HAMBURGER ICON - Android Studio Tutorial

--- README.md
+++ README.md
@@ -3,4 +3,9 @@
 Tutorial Video
 1. Navigation Drawer with Fragments Part 1 - MENU AND ACTIVITY THEME - Android Studio Tutorial
     - https://youtu.be/fGcMLu1GJEc
+2. Navigation Drawer with Fragments Part 2 - LAYOUT AND HAMBURGER ICON - Android Studio Tutorial
+    - https://youtu.be/zYVEMCiDcmY
 
+
+Problem:
+1. rotate the hamburger icon when user pull out the drawer
--- app/src/main/java/com/example/naviagtiondrawerexample/MainActivity.java
+++ app/src/main/java/com/example/naviagtiondrawerexample/MainActivity.java
@@ -1,14 +1,38 @@
 package com.example.naviagtiondrawerexample;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    private DrawerLayout drawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        drawer = findViewById(R.id.drawer_layout);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        toggle.syncState();
+    }
+
+    @Override
+    public void onBackPressed() {
+        // IF the drawer is open, THEN close the drawer 1st bfr close the activity as normal
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 }
